48ecb2f819f4ca63ed97d814cba4fe6bc5a3a2f4
Steve M <steve@yarrumevets.com>
2024-04-06 23:33:40 -0700

    Implemented keep alive mechanism and updated other code to use the new data structure built around it

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = () => {
   var elementChatLog = document.getElementById("chat-log");
   var elementInputText = document.getElementById("input-text");
   var elementSubmitButton = document.getElementById("submit-button");
@@ -26,23 +26,49 @@ window.onload = function () {
   const protocol = window.location.protocol === "https:" ? "wss" : "ws";
 
   const connection = new WebSocket(`${protocol}://${host}/sockchatsock`); // wss://localhost/sockchatsock
-  connection.onopen = function () {
+  connection.onopen = () => {
     addMessage(
       "Connected to server...",
       "",
       new Date(Date.now()).toUTCString()
     );
+
+    // Send a ping to the client every 50 seconds to keep the connection alive.
+    // ! Make sure this number (ms) is less than the value set for proxy_read_timeout (sec * 1000) in nginx.conf, default 60s.
+    const keepAliveInterval = setInterval(() => {
+      if (connection.readyState === WebSocket.OPEN) {
+        connection.send(JSON.stringify({ type: "ping" }));
+      }
+    }, 50000); // 50 seconds. Assumes nginx default proxy_read_timeout of 60 seconds
+
+    connection.onclose = () => {
+      // Necessary when the app can function after the connection is closed.
+      clearInterval(keepAliveInterval);
+    };
+
+    connection.onclose = (e) => {
+      console.log(
+        "\uD83D\uDC94 WEBSOCKET CONNECTION HAS CLOSED \uD83D\uDC94",
+        e
+      );
+    };
   };
 
   // Receive update from server.
-  connection.onmessage = function (message) {
+  connection.onmessage = (message) => {
     var data = JSON.parse(message.data);
+    // Catch pong responses for keep alive mechanism.
+    if (data.type === "pong") {
+      console.log("Pong!"); // Uncomment to monitor keepalive.
+      return;
+    }
+    console.log("message: ", data);
     addMessage(data.user, data.message, data.timestamp);
   };
-
   // Send message to server.
-  elementSubmitButton.onclick = function () {
-    var message = elementInputText.value;
+  elementSubmitButton.onclick = () => {
+    var messageText = elementInputText.value;
+    const message = JSON.stringify({ messageText, type: "" });
     connection.send(message);
     elementInputText.value = "";
   };
--- webserver.js
+++ webserver.js
@@ -23,30 +23,25 @@ websocketServer.on("connection", (websocket) => {
   guestCount += 1;
   sockets.push(websocket);
   websocket.on("message", (message) => {
+    const messageObj = JSON.parse(message);
+
+    // Handle keep alive pings.
+    if (messageObj.type === "ping") {
+      websocket.send(JSON.stringify({ type: "pong" }));
+      return;
+    }
+
+    const now = Date.now();
+    const timeStamp = new Date(now).toUTCString();
+    const newMessage = JSON.stringify({
+      timestamp: timeStamp,
+      user: websocket.guestId,
+      message: messageObj.messageText,
+      type: messageObj.type,
+    });
+
     sockets.forEach((sock) => {
-      const now = Date.now();
-      const timeStamp = new Date(now).toUTCString();
-      const newMessage = JSON.stringify({
-        timestamp: timeStamp,
-        user: websocket.guestId,
-        message: message,
-      });
       sock.send(newMessage);
     });
-  });
-
-  // Send a ping to the client every 30 seconds
-  const interval = setInterval(() => {
-    if (ws.readyState === WebSocket.OPEN) {
-      ws.ping("keepalive");
-    }
-  }, 50000); // 50 seconds. Assumes nginx default proxy_read_timeout of 60 seconds
-
-  ws.on("pong", function incoming(data) {
-    console.log(`Pong received from client: ${data}`);
-  });
-
-  ws.on("close", function clear() {
-    clearInterval(interval);
   });
 });
